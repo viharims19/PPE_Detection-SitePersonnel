abfe5251349f971ce0967c92039e1253b2047083
viharims19 <sai.vihari95@gmail.com>
2024-07-26 23:20:58 +0530

    Add requirements

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'ppe_detector',
+    version= '0.0.0',
+    author= 'vihari',
+    author_email= 'sai.vihari95@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
